cc3057a0c0ed11c8b3ff96e7683bdd3f7d8bd1af
Ethan <ewvansc@pointpark.edu>
2025-09-09 16:02:47 -0400

    CS5 assignment

---
+++ CS5/Pages/Private.cshtml.cs
@@ -0,0 +1,13 @@
+@page
+@model PrivateModel
+@{
+    ViewData["Title"] = "Private";
+}
+
+<div class="text-center">
+    <h1 class="display-4">Private Page</h1>
+    <p>Learn about <a href="https://learn.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>
+</div>
+<body>
+<h1>Welcome to my first C# Page</h1>
+</body>
