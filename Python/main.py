d98febcd6c438871baf76d260908edc30bb183a4
Ethan <ewvansc@pointpark.edu>
2025-09-16 12:07:03 -0400

    Python 10

---
+++ Python/main.py
@@ -0,0 +1,41 @@
+
+#print("hello world")
+
+
+
+#length = float(input("Enter the length of the rectangle: "))
+#width = float(input("Enter the width of the rectangle: "))
+#area = length * width
+#print("The area of the rectangle is:", area)
+
+
+#num = input("Enter a number: ")
+#if num % 2 == 0:
+#   print(num, "is even")
+#else:
+#    print(num, "is odd")
+
+
+#num1 = input("Enter the first number: ")
+#num2 = input("Enter the second number: ")
+#num3 = input("Enter the third number: ")
+#minvalue = min(num1,num2,num3)
+#print("The min number is:", minvalue)
+
+## Assignment, figure out reverse
+#string = input("Enter a string value: ")
+
+#if string == string[::-1]:
+#   print("Yay it worked")
+#else:
+#    print("Sorry we lost")
+
+## Assignment, figure out reverse string
+
+string = input("Enter a string value: ")
+
+reverseString = string[::-1]
+
+print("Reverse String:", reverseString)
+
+#The above code worked via Online Python
